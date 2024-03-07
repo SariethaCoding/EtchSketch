4f9beac6109761cb17f9bdaef0f50e042e1a5ffb
Sarietha <sarietha.coding@gmail.com>
2024-03-07 17:09:08 +0200

    Added eventlistener for mouse over effect

--- etch.js
+++ etch.js
@@ -8,6 +8,13 @@ for(let i = 0; i < 16; i++) {
     for(let j = 0; j < 16; j++) {
         const newDivBlock = document.createElement("div");
         newDivBlock.classList.add("square");
+
+        // add eventListener to create a colour effect with mouseOver
+        newDivBlock.addEventListener("mouseover", function (e) {
+            //console.log(e);
+            e.target.style.background = 'white';
+        });
+        
         document.getElementById("row"+i).appendChild(newDivBlock);
     }
 }
