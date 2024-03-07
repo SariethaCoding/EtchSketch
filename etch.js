4a325285deefd21df3e176f3cf54d09a2c52e153
Sarietha <sarietha.coding@gmail.com>
2024-03-07 18:51:38 +0200

    Added choose size function and made the drawing of the grid into a function

--- etch.js
+++ etch.js
@@ -1,11 +1,26 @@
-// Create a grid of 16 x 16 squares...
-for(let i = 0; i < 16; i++) {
+function createGrid(squares) {
+
+console.log("Creating grid.... number of squares is : " + squares);
+
+if (squares == null) {
+    squares = 16;
+}
+
+console.log("squares are now : " + squares);
+// Clean the previous grid!
+document.getElementById("grid").textContent = "";
+
+
+// Create a grid of squares...
+for(let i = 0; i < squares; i++) {
     const newRowDiv = document.createElement("div");
     newRowDiv.classList.add("row");
     newRowDiv.id = ("row"+i);
     document.getElementById("grid").appendChild(newRowDiv);
+    // set the height depending on number of squares
+    document.getElementById("row"+i).style.height = (100/squares+"%");
 
-    for(let j = 0; j < 16; j++) {
+    for(let j = 0; j < squares; j++) {
         const newDivBlock = document.createElement("div");
         newDivBlock.classList.add("square");
 
@@ -17,4 +32,25 @@ for(let i = 0; i < 16; i++) {
         
         document.getElementById("row"+i).appendChild(newDivBlock);
     }
+}
+
+}
+
+function getNumberOfSquares() {
+    console.log("get the number of squares");
+
+    let squares = prompt("Please enter number of squares you want per row for the new grid : ");
+
+    // Put in check to make sure it's a valid number......
+
+    if (squares == null || squares == "") {
+        console.log("Didn't enter anything")
+    } 
+    else if (squares > 100) {
+        squares = prompt("Please enter a number smaller than 100")
+    }
+    else {
+        createGrid(squares);
+    }
+    
 }
